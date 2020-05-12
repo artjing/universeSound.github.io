d0f0c12bb3abf73c2d32be64646d0eac5586faab
510599228@qq.com <510599228@qq.com>
2020-05-12 11:55:41 -0700

    cc

--- intro.js
+++ intro.js
@@ -6,15 +6,37 @@ function setup() {
 
   rectMode(CENTER);
   
-  textSize(38);
+  textSize(30);
   fill(255);
   let t = 'Renosance with universe';
   let tWith = textWidth(t);
   text(t,(width-tWith)/2,100);
   
+  
+  textSize(10);
+  fill(255,200);
+
+  let t1 = 'This is a sound website that can interact with body movements. Please keep a little distance with your camera, and try to move your body. ';
+  let t11 = 'You can chose a planet sound as background sound, and make a resonance with universe. There are three trigger types seperately base on your position, gesture and path.';
+  let t1With = textWidth(t1);
+  let t11With = textWidth(t11);
+
+  text(t1,(width-t1With)/2,(height-100)/2-130);
+  text(t11,(width-t11With)/2,(height-100)/2-110);
+
+  textSize(16);
+  fill(255);
+  let t2 = 'Keep a distance with your camera';
+  let t3 = 'Move your body'
+  let t2With = textWidth(t2);
+  let t3With = textWidth(t3);
+
+  text(t2,(width-t2With)/2,(height-100)/2 + 150);
+  text(t3,(width-t3With)/2,(height-100)/2 + 175);
+  
   imageButton = createImg('back.png');
-  imageButton.position((width-280)/2,(height-280)/2,0);
-  imageButton.size(280,280);
+  imageButton.position((width-100)/2,(height-100)/2,0);
+  imageButton.size(100,100);
   
   var button = createButton('Start');
   
@@ -25,9 +47,9 @@ function setup() {
   button.style('color', tcol);
 
   var W = 150;
-  var H = 30;
+  var H = 40;
   button.size(W,H);
-  button.position((width-W)/2, height-100);
+  button.position((width-W)/2, height-120);
   button.mousePressed(jumpToIndex);
   
 }
