cda9af042a42d757fd9bfd64b53e3b7ed6330ed7
510599228@qq.com <510599228@qq.com>
2020-05-12 00:43:11 -0700

    add new

---
+++ intro.js
@@ -0,0 +1,40 @@
+function setup() {
+  
+  createCanvas(windowWidth, windowHeight);
+  
+  background(0);
+
+  rectMode(CENTER);
+  
+  textSize(38);
+  fill(255);
+  let t = 'Renosance with universe';
+  let tWith = textWidth(t);
+  text(t,(width-tWith)/2,100);
+  
+  imageButton = createImg('back.png');
+  imageButton.position((width-220)/2,(height-220)/2,0);
+  imageButton.size(220,220);
+  
+  var button = createButton('Start');
+  
+  let col = color(0);
+  let tcol = color(255);
+
+  button.style('background-color', col);
+  button.style('color', tcol);
+
+  var W = 150;
+  var H = 30;
+  button.size(W,H);
+  button.position((width-W)/2, height-120);
+  button.mousePressed(jumpToIndex);
+  
+}
+
+function draw() {
+}
+
+function jumpToIndex(){
+    window.location.href="index.html"
+}
